0704cec92e95000683b6888e58b9a0fe8e504bde
RachaelChar <charewicz.r@gmail.com>
2020-03-03 19:37:23 -0500

    utilize Moment.js in ScanJumbotron

--- client/src/components/ScanJumbotron/index.js
+++ client/src/components/ScanJumbotron/index.js
@@ -1,24 +1,27 @@
 import React from "react";
+import Moment from "react-moment";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import './style.css';
 
 export default function ScanJumbotron() {
 
   return (
-    <Jumbotron className="shadow p-3 mb-5 bg-white border border-secondary">
-      <div id="currentTime" className="text-center">
-        <h1 className="font-weight-bold">12:00pm</h1>
-        <h3>Saturday, February 29th</h3>
-        <form id="scan-form" className="">
-          <div className="mt-4" onkeypress="">
-            <input id="badgeID"
-              className="border"
-              type="text" placeholder="Scan Badge" />
-            <div className="mt-4">
-              <span className="popuptext" id="myPopup">ID Scanned</span>
+    <div>
+      <Jumbotron id="jumbotron" className="shadow p-3 mb-5 bg-white border border-secondary" >
+        <div id="currentTime" className="text-center">
+
+          <Moment id="scan-clock" format="LT"></Moment> <br />
+          <Moment id="scan-date" format="MMMM Do, YYYY"></Moment>
+
+          <form id="scan-form" className="">
+            <div className="mt-4" onkeypress="">
+              <input id="badgeID"
+                className="border mb-3"
+                type="text" placeholder="Scan Badge" />
             </div>
-          </div>
-        </form>
-      </div>
-    </Jumbotron>
+          </form>
+        </div>
+      </Jumbotron>
+    </div>
   )
 }
