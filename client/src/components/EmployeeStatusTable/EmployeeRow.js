767c4e383edb61c20508643b5fc72dbe2b0f0636
Kyle Jones <kylejonesnh@gmail.com>
2020-03-03 19:40:26 -0500

    added axios

---
+++ client/src/components/EmployeeStatusTable/EmployeeRow.js
@@ -0,0 +1,13 @@
+ import React from 'react';
+ 
+ export default function EmployeeRow() {
+ 
+ return(
+ <tr>
+    <td><FirstName /></td>
+    <td><LastName /></td>
+    <td><Status /></td>
+    <td><Button variant="primary">View</Button></td>
+</tr>
+ )
+ }
--- client/src/components/EmployeeStatusTable/index.js
+++ client/src/components/EmployeeStatusTable/index.js
@@ -1,10 +1,41 @@
 import React from "react";
+import EmployeeRow from './EmployeeRow';
 
 export default function EmployeeStatusTable() {
 
   return (
     <div>
-      EmployeeStatusTable
-      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Status</th>
+            <th>Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Brandon</td>
+            <td>Fall</td>
+            <td>Clocked-In</td>
+            <td><Button variant="primary">View</Button></td>
+          </tr>
+          <tr>
+            <td>Kyle</td>
+            <td>Jones</td>
+            <td>Clocked-In</td>
+            <td><Button variant="primary">View</Button></td>
+          </tr>
+          <tr>
+            <td>Marissa</td>
+            <td>Fortier</td>
+            <td>Clocked-In</td>
+            <td><Button variant="primary">View</Button></td>
+          </tr>
+          <EmployeeRow />
+        </tbody>
+      </Table>
+    </div>
   )
 }
