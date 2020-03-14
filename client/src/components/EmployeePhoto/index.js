f34fb528b56de6bc537aa390279fe90cea8a42fe
bfall-debug <bfall@invengeon.com>
2020-03-14 11:15:16 -0400

    picture sized better

--- client/src/components/EmployeePhoto/index.js
+++ client/src/components/EmployeePhoto/index.js
@@ -5,11 +5,17 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 export default function EmployeePhoto(props) {
+  var divStyle = {
+    width: '300px',
+    height: '300px'
+  };
+
+
   return (
     <Container>
       <Row>
         <Col xs={6} md={4}>
-          <Image alt="employee photo" src={`${props.employeeInfo.picture_fp}`} roundedCircle />
+          <Image alt="employee photo" style={divStyle} src={`${props.employeeInfo.picture_fp}`} roundedCircle />
         </Col>
       </Row>
     </Container>
