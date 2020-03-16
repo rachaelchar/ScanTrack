55ebb044e224b10871f0a7f0defa43a6c1f5be45
Marissa Fortier <marissafortier@gmail.com>
2020-03-15 22:16:18 -0400

    fixed styling issues on sidenav buttons

--- client/src/components/SideNav/index.js
+++ client/src/components/SideNav/index.js
@@ -12,13 +12,13 @@ export default function SideNav() {
         <hr className="mb-0"></hr>
 
         <div className="sidebar">
-          <button href="/" className="py-3 sidelinks sideButton"><i id="icon" className="fa fa-home ml-4"></i>Home</button>
+          <button href="/" className="py-3 sidelinks" id="sideButton"><i id="icon" className="fa fa-home ml-4"></i>Home</button>
           <hr className="my-0"></hr>
-          <button href="/profile" className="py-3 sidelinks sideButton"><i id="icon" className="fa fa-cog ml-4" aria-hidden="true"></i>View My Profile</button>
+          <button href="/profile" className="py-3 sidelinks" id="sideButton"><i id="icon" className="fa fa-cog ml-4" aria-hidden="true"></i>View My Profile</button>
           <hr className="my-0"></hr>
-          <button href="/register" className="py-3 sideButton"><i id="icon" className="fa fa-fw fa-user ml-4"></i>Register New Employee</button>
+          <button href="/register" className="py-3" id="sideButton"><i id="icon" className="fa fa-fw fa-user ml-4"></i>Register New Employee</button>
           <hr className="my-0"></hr>
-          <button href="/print" className="py-3 sideButton"><i id="icon" className="fa fa-print ml-4"></i>Print Last Weeks Hours</button>
+          <button href="/print" className="py-3" id="sideButton"><i id="icon" className="fa fa-print ml-4"></i>Print Last Weeks Hours</button>
           <hr className="my-0"></hr>
         </div>
       </div>
